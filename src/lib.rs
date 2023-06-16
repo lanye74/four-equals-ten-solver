18ce4d9c6049e67e2f544da36905ec0e37a1ba54
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2023-06-16 18:00:41 -0400

    separate logic from main.rs into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+mod configurator;
+pub use configurator::{Config, Configurator};
+
+mod io_reader;
+
+mod solver;
+
+
+
+// TODO: make print_solutions part of Config
+pub fn run(config: &Config, print_solutions: bool) {
+	let output = solver::brute_force(config);
+
+	if output.solutions.is_empty() {
+		println!("No solutions found!");
+		println!("Time taken: {:?}", output.time_taken);
+		println!("Solutions considered: {}", output.solutions_considered);
+
+		return;
+	}
+
+
+	let solutions_len = output.solutions.len();
+
+	println!("Solution{} found!:", if solutions_len > 1 {"s"} else {""});
+
+	if print_solutions == true {
+		for sol in output.solutions {
+			println!("{}", sol);
+		}
+	}
+
+
+	if solutions_len > 1 {
+		println!("Total: {}", solutions_len);
+	}
+
+
+	println!("Time taken: {:?}", output.time_taken);
+	println!("Solutions considered: {}", output.solutions_considered);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-mod io_reader;
-
 mod profile;
 use profile::is_using_profile;
 
-mod configurator;
-use configurator::Configurator;
-
-mod solver;
+use four_equals_ten::{Configurator, run};
 
 
 
 fn main() {
-	// TODO: cleanup main
 	let mut configurator = Configurator::new();
 
 	// don't print if debugging via flamegraph
@@ -20,34 +14,5 @@ fn main() {
 
 	let config = configurator.build_config();
 
-	let output = solver::brute_force(&config);
-
-
-	if output.solutions.is_empty() {
-		println!("No solutions found!");
-		println!("Time taken: {:?}", output.time_taken);
-		println!("Solutions considered: {}", output.solutions_considered);
-
-		return;
-	}
-
-
-	let solutions_len = output.solutions.len();
-
-	println!("Solution{} found!:", if solutions_len > 1 {"s"} else {""});
-
-	if print_solutions == true {
-		for sol in output.solutions {
-			println!("{}", sol);
-		}
-	}
-
-
-	if solutions_len > 1 {
-		println!("Total: {}", solutions_len);
-	}
-
-
-	println!("Time taken: {:?}", output.time_taken);
-	println!("Solutions considered: {}", output.solutions_considered);
+	run(&config, print_solutions);
 }
